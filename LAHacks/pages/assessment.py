2874789635fc62296462f29577ce1e250bcb6adc
jerryznr99 <jerryznr99@gmail.com>
2024-04-20 20:18:07 -0700

    styling changes

--- LAHacks/pages/assessment.py
+++ LAHacks/pages/assessment.py
@@ -5,12 +5,28 @@ def assessment() -> rx.Component:
     # Simple text display and a button to proceed. Actual data collection would need more functionality.
     return rx.center(
         rx.vstack(
-            rx.heading("Please describe your symptoms"),
-            rx.text("Describe your symptoms in detail:"),
-            rx.button("Submit", on_click=lambda: rx.redirect("/waiting")),
+            rx.text(
+                "Please describe your symptoms", 
+                size="8", 
+                font_family="Metropolis", 
+                font_weight="bold",),
+            rx.text(
+                "Describe your symptoms in detail:",
+                align="center", 
+                size="6",
+                width="800px",),
+            rx.button(
+                "Submit",
+                on_click=lambda: rx.redirect("/waiting"),
+                size="3",
+                font_family="Metropolis",
+                background_color="#1B3EF3",
+            ),
             align="center",
             spacing="5"
         ),
+        align="center",
+        spacing="7",
         height="100vh"
     )
 
